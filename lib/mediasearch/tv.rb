39d6d5177b2dd43e572e8ff9c2c8f3857cfc5a06
Benjamin Hsieh <bhsiehh@gmail.com>
2014-04-07 22:25:17 -0700

    Set up TVDB config and search

--- lib/mediasearch/tv.rb
+++ lib/mediasearch/tv.rb
@@ -2,5 +2,19 @@ require 'tvdb_party'
 
 module Mediasearch
   class Tv < Media
+    def self.check_client
+      if Mediasearch.tvdb_ready?
+        @client = TvdbParty::Search.new(Mediasearch.tvdb_config.api_key)
+        yield
+      else
+        raise ArgumentError, 'TVDB not configured.'
+      end
+    end
+
+    def self.search(query)
+      check_client do
+        @client.search(query)
+      end
+    end
   end
 end
